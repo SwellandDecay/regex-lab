1c41c4898c004812bc63b4e9787d8b237213d1c1
Garrett <Garrett.Bodley@gmail.com>
2020-11-30 15:38:37 -0500

    commit tree 49bd75581d13737bcd5dac07ed16beeabdb3bcdc

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    /\A[aeiouAEIOU]/ === word
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text = text
+    text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
+    if /[A-Z]/ === text[0] &&  /[.,?:;!]/ === text[-1]
+        return true
+    else
+        return false 
+    end
+
 end
 
 def valid_phone_number?(phone)
-
+    /\d{3}\D*\d{3}\D*\d{4}/ === phone
 end
